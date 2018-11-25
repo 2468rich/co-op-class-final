031ee6a1fb527209f81af248ddf37fbed23b994a
Cam <skubikcj@mail.uc.edu>
2018-11-25 15:28:18 -0500

    Re-adding Seth's files.

---
+++ Final_Project/Final_Project/SethScraper.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Final_Project
+{
+    partial class SethsScraper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.movieRankings = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.movieRankings)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // movieRankings
+            // 
+            this.movieRankings.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.movieRankings.Location = new System.Drawing.Point(12, 12);
+            this.movieRankings.Name = "movieRankings";
+            this.movieRankings.Size = new System.Drawing.Size(776, 426);
+            this.movieRankings.TabIndex = 0;
+            // 
+            // SethsScraper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.movieRankings);
+            this.Name = "SethsScraper";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.movieRankings)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView movieRankings;
+    }
+}
---
+++ Final_Project/Final_Project/SethScraper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace Final_Project
+{
+    public class NameAndScore
+    {
+        public string Name { get; set; }
+        public string Score { get; set; }
+    }
+
+    public partial class SethsScraper: Form
+    {
+        DataTable table;
+        HtmlWeb web = new HtmlWeb();
+        public SethsScraper()
+        {
+            InitializeComponent();
+            InitTable();
+        }
+
+        private void InitTable()
+        {
+            table = new DataTable("MovieRankingsTable");
+            table.Columns.Add("Name", typeof(string));
+            table.Columns.Add("Ranking", typeof(string));
+            movieRankings.DataSource = table;
+        }
+
+        private async Task<List<NameAndScore>> MovieRankingsFromPage ()
+        {
+            var doc = await Task.Factory.StartNew(() => web.Load("https://www.imdb.com/chart/top?ref_=nv_wl_img_3"));
+
+            var nameNodes = doc.DocumentNode.SelectNodes("//*[@id=\"main\"]//div//span//div//div//div//table//tbody//tr//td//a"); // may have to remove tbody
+            var innerNames = nameNodes.Select(node => node.InnerText);
+            var names = innerNames.Where((x, i) => (i + 1) % 2 == 0);
+
+            var scoreNodes = doc.DocumentNode.SelectNodes("//*[@id=\"main\"]//div//span//div//div//div//table//tbody//tr//td//strong");
+            var scores = scoreNodes.Select(node => node.InnerText);
+
+            return names.Zip(scores, (name, score) => new NameAndScore() { Name = name, Score = score }).ToList();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var rankings = await MovieRankingsFromPage();
+            foreach(var ranking in rankings)
+            {
+                table.Rows.Add(ranking.Name, ranking.Score);
+            }
+        }
+    }
+}
